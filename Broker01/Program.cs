86a89e70dbb74e31da16a4c120243357dfb7ee8e
unknown <belesev@gmail.com>
2017-08-19 22:03:52 +0300

    sln

---
+++ Broker01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuikSharp;
+
+namespace Broker01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quik = new Quik(Quik.DefaultPort, new InMemoryStorage());    // инициализируем объект Quik
+            var isServerConnected = quik.Service.IsConnected().Result;
+        }
+    }
+}
