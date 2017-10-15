42216f485f9c22ae1f1ae762075b9b09af44b3df
belesev <belesev@gmail.com>
2017-10-15 20:08:01 +0300

    succcessfull connect

---
+++ Broker01/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using log4net;
+using log4net.Config;
+
+namespace Broker01
+{
+    public static class Logger
+    {
+        private static ILog log = LogManager.GetLogger("LOGGER");
+
+        public static ILog Log
+        {
+            get { return log; }
+        }
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure(new FileInfo("..\\..\\Config\\log4net.config"));
+        }
+    }
+}
---
+++ Broker01/XmlConfigReader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Broker01
+{
+    public class XmlConfigReader
+    {
+        private readonly string fileName;
+
+        public XmlConfigReader(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public List<string> ReadList()
+        {
+            var result = new List<string>();
+            var text = File.ReadAllText(fileName);
+            using (var reader = XmlReader.Create(new StringReader(text)))
+            {
+                while (reader.ReadToFollowing("i"))
+                {
+                    reader.Read();
+                    var value = reader.Value;
+                    result.Add(value);
+                }
+            }
+            return result;
+        }
+    }
+}
